8c172a69be16f8369fc97f95d36fa7a3c7227851
vilczyca <jkoszn@gmail.com>
2025-03-15 23:10:24 +0100

    Console version + experiments

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Projekty/dining_philosophers
+# Install script for directory: E:/Projekty/SO2/dining_philosophers
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "E:/Projekty/dining_philosophers/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "E:/Projekty/SO2/dining_philosophers/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- main.cpp
+++ main.cpp
@@ -1,7 +1,163 @@
 #include <iostream>
+#include <thread>
+#include <string>
+#include <mutex>
+#include <vector>
+
+class Philosopher;
+class Fork;
+
 using namespace std;
 
-int main() {
-    cout << "Hello world!" << endl;
-    return 0;
+static bool finished = false;
+
+class Fork {
+public:
+    mutex mtx;
+};
+
+
+class Philosopher {
+private:
+    static int staticID;
+    const int ID;
+
+    thread myThread;
+
+    int eatingTime = 3,
+        thinkingTime = 5;
+
+    Fork* leftFork;
+    Fork* rightFork;
+
+    void doWork() {
+        while (!finished) {
+            think();
+            tryToEat_Assymetry();
+        }
+    };
+
+    bool tryToEat() {
+        // left
+        leftFork->mtx.lock();
+        if (rightFork->mtx.try_lock()) {
+            eat();
+            rightFork->mtx.unlock();
+            leftFork->mtx.unlock();
+            return true;
+        }
+        leftFork->mtx.unlock();
+        return false;
+    };
+
+    bool tryToEat_Assymetry() {
+        if (ID % 2 == 0) {          // even
+            rightFork->mtx.lock();
+            leftFork->mtx.lock();
+            eat();
+            leftFork->mtx.unlock();
+            rightFork->mtx.unlock();
+        } else {                    // odd
+            leftFork->mtx.lock();
+            rightFork->mtx.lock();
+            eat();
+            rightFork->mtx.unlock();
+            leftFork->mtx.unlock();
+        }
+        return true;
+    };
+
+    bool tryToEat_WithWaiter() {
+        lock(leftFork->mtx, rightFork->mtx);
+        lock_guard<mutex> leftLock(leftFork->mtx, adopt_lock);
+        lock_guard<mutex> rightLock(rightFork->mtx, adopt_lock);
+
+        eat();
+        return true;
+    }
+
+
+
+    void eat() {
+        // TODO: eating animation
+        cout << ID << " is eating..." << endl;
+        this_thread::sleep_for(3s);
+    };
+
+    void think() {
+        // TODO: thinking animation
+        cout << ID << " is thinking..." << endl;
+        this_thread::sleep_for(2s);
+    };
+
+
+public:
+    // Constructors and Destructors
+    Philosopher() : ID(staticID++) {};
+    Philosopher(Fork* leftFork, Fork* rightFork): leftFork(leftFork), rightFork(rightFork), ID(staticID++) {};
+    ~Philosopher() {
+        stop();
+    }
+
+    // Starts the thread
+    void start() {
+        myThread = thread(&Philosopher::doWork, this);
+        cout << "Philosopher " << this->ID << " arrived to the dinner!" << endl;
+    }
+
+    // Stops the thread
+    void stop() {
+        if (myThread.joinable()) {
+            myThread.join();
+            cout << "Philosopher " << ID << " leaved from the dinner!" << endl;
+        }
+    }
+};
+
+int Philosopher::staticID = 0;
+
+int main(int argc, char* argv[]) {
+    int n = 5;  // classic number
+
+    if (argc > 1)
+        n = stoi(argv[1]);
+
+    // Creating dinner
+    vector<Fork> forks(n);
+    vector<unique_ptr<Philosopher>> philosophers;
+
+    for (int i = 0; i < n; i++) {
+        philosophers.emplace_back(make_unique<Philosopher>(&forks[i], &forks[(i + 1) % n]));
+        philosophers.back()->start();
+    }
+
+    cin.get();
+    cout << "Dinner is over!" << endl << endl;
+    finished = true;
+
+    // Cleaning
+    for (auto& philosopher : philosophers) {
+        philosopher->stop();
+    }
 }
+
+
+// https://www.youtube.com/watch?v=FYUi-u7UWgw
+
+
+/*
+  o
+ /|\
+ / \
+                               o
+    o        o        o       /|\
+   /|\  Y   /|\     Y/|\Y    _===__________
+   / \      / \      / \    |            |
+
+  o     ,o      o      o.    o
+Y/|\Y   '|\Y  Y/|\Y  Y/|'  Y/|\Y
+ / \    / \    / \    / \   / \
+
+
+
+ */
